273fb92f4d8fd89ad7508b33c0071b3925210eaf
nateharrell <nateharrell@CIS-RDS4>
2021-03-08 21:13:21 -0600

    Add project files.

---
+++ OAHeLP Database Project/UI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient; // need this for SQL
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAHeLP_Database_Project
+{
+    /// <summary>
+    /// User Interface
+    /// </summary>
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+    }
+}

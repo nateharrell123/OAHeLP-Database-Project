70dc59b5ba97ba0cbf40ebf900f520c1ea94437f
Jack2 <jackfrost0@ksu.edu>
2021-03-29 13:28:59 -0500

    Made a new class to develope in, but I'm not sure if it's needed

--- OAHeLP Database Project/SearchAndSort.cs
+++ OAHeLP Database Project/SearchAndSort.cs
@@ -9,7 +9,26 @@ namespace OAHeLP_Database_Project
     class SearchAndSort
     {
 
+        /// <summary>
+        /// this dictionary tracks each subject's score compared to test data
+        /// </summary>
+        Dictionary<int, int> subjectIDAndScores = new Dictionary<int, int>();
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SearchAndSort()
+        {
+
+        }//searchandsort
+
+
+        public Dictionary<int,int> SearchDB()
+        {
+
+
+            return subjectIDAndScores;
+        }
 
 
     }

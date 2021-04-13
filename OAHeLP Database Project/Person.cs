a115ffce2ac88d7de55be12f87ac08a0f071312a
nateharrell <nateharrell@CIS-RDS4>
2021-04-12 22:10:56 -0500

    asdfd

--- OAHeLP Database Project/Person.cs
+++ OAHeLP Database Project/Person.cs
@@ -14,13 +14,14 @@ namespace OAHeLP_Database_Project
         public string FirstName { get; set; }
         public string MiddleNames { get; set; }
         public string LastName { get; set; }
-        public string FullName => FirstName + MiddleNames + LastName;
+        public string FullName { get; set; }
 
         public Person(string firstName, string middleNames, string lastName)
         {
             FirstName = firstName;
             MiddleNames = middleNames;
             LastName = lastName;
+            //FullName = firstName + '\t' + middleNames + '\t' + lastName;
         }
 
         public override string ToString() { return $"{FirstName} \t {MiddleNames} \t {LastName}"; }

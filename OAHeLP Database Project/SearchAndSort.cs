060c01f7365d1bc23f92f5fdd4682ae852843269
jackfrost0 <jackfrost0@ksu.edu>
2021-05-03 14:27:24 -0500

    Seperated the PhonemeGenerator methods from the actual SearchAndSort

--- OAHeLP Database Project/SearchAndSort.cs
+++ OAHeLP Database Project/SearchAndSort.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;
-//using Microsoft.AspNet.SignalR.Infrastructure;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Device.Location;
-using System.Speech.Recognition;
-using System.Speech.Synthesis;
+
 
 namespace OAHeLP_Database_Project
 {
-    //merging was being odd, so copy and pasting from John branch
 
     public class SearchAndSort
     {
@@ -440,9 +434,10 @@ namespace OAHeLP_Database_Project
             //make this compatible with all character types? I think it currently is, but more testing is necessary to be entirely sure
 
             //first step is to pheoneticise the names
+            
 
-            string inputPhone = GetPronunciationFromText(inputName);
-            string dbPhone = GetPronunciationFromText(dbName);
+            string inputPhone = PhonemeGenerator.GetPronunciationFromText(inputName);
+            string dbPhone = PhonemeGenerator.GetPronunciationFromText(dbName);
 
 
             //next is to find the Levenshtein distance between the names
@@ -466,7 +461,6 @@ namespace OAHeLP_Database_Project
         {
             return (newMax - newMin) * (unscaledNum - previousMin) / (previousMax - previousMin) + newMin;
         }
-
 
         /// <summary>
         /// This method computes the levenshteinDistance between two strings
@@ -523,131 +517,10 @@ namespace OAHeLP_Database_Project
 
         // Credit for method of retrieving IPA pronunciation from a string goes to Casey Chesnut (http://www.mperfect.net/speechSamples/)
 
-        private static string recoPhonemes;
 
-        /// <summary>
-        /// This method is for getting the word phones from a given string
-        /// </summary>
-        /// <param name="MyWord">The word (string) that needs to be phoeneticised</param>
-        /// <returns>the phones for the given input</returns>
-        private static string GetPronunciationFromText(string MyWord)
-        {
-            //this is a trick to figure out phonemes used by synthesis engine
-
-            //txt to wav
-            using (MemoryStream audioStream = new MemoryStream())
-            {
-                using (SpeechSynthesizer synth = new SpeechSynthesizer())
-                {
-                    synth.SetOutputToWaveStream(audioStream);
-                    synth.Speak(MyWord);
-                    synth.SetOutputToNull();
-                    audioStream.Position = 0;
-
-                    //now wav to txt (for reco phonemes)
-                    recoPhonemes = String.Empty;
-                    GrammarBuilder gb = new GrammarBuilder(MyWord);
-                    Grammar g = new Grammar(gb); //TODO the hard letters to recognize are 'g' and 'e', but it gets them a large majority of the time, at least 90%
-                    SpeechRecognitionEngine reco = new SpeechRecognitionEngine();
-                    reco.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(reco_SpeechHypothesized);
-                    reco.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(reco_SpeechRecognitionRejected);
-                    reco.UnloadAllGrammars();
-                    reco.LoadGrammar(g);
-                    reco.SetInputToWaveStream(audioStream);
-                    RecognitionResult rr = reco.Recognize();
-                    reco.SetInputToNull();
-                    if (rr != null)
-                    {
-                        recoPhonemes = StringFromWordArray(rr.Words, WordType.Pronunciation);
-                    }
-                    return recoPhonemes;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Creates a string given an array of word units
-        /// </summary>
-        /// <param name="words"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        private static string StringFromWordArray(ReadOnlyCollection<RecognizedWordUnit> words, WordType type)
-        {
-            string text = "";
-            foreach (RecognizedWordUnit word in words)
-            {
-                string wordText = "";
-                if (type == WordType.Text || type == WordType.Normalized)
-                {
-                    wordText = word.Text;
-                }
-                else if (type == WordType.Lexical)
-                {
-                    wordText = word.LexicalForm;
-                }
-                else if (type == WordType.Pronunciation)
-                {
-                    wordText = word.Pronunciation;
-                }
-                else
-                {
-                    throw new InvalidEnumArgumentException(String.Format("[0}: is not a valid input", type));
-                }
-                //Use display attribute
-
-                if ((word.DisplayAttributes & DisplayAttributes.OneTrailingSpace) != 0)
-                {
-                    wordText += " ";
-                }
-                if ((word.DisplayAttributes & DisplayAttributes.TwoTrailingSpaces) != 0)
-                {
-                    wordText += "  ";
-                }
-                if ((word.DisplayAttributes & DisplayAttributes.ConsumeLeadingSpaces) != 0)
-                {
-                    wordText = wordText.TrimStart();
-                }
-                if ((word.DisplayAttributes & DisplayAttributes.ZeroTrailingSpaces) != 0)
-                {
-                    wordText = wordText.TrimEnd();
-                }
-
-                text += wordText;
-
-            }
-            return text;
-        }
-
-        /// <summary>
-        /// Event for when the recognition engine has a guess as to the phonemes for a word
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void reco_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
-        {
-            recoPhonemes = StringFromWordArray(e.Result.Words, WordType.Pronunciation);
-        }
-
-        /// <summary>
-        /// Event for when the recognition engine regects the given wav (default error message produced)
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private static void reco_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
-        {
-            recoPhonemes = StringFromWordArray(e.Result.Words, WordType.Pronunciation);
-        }
-
+        
     }//class
 
-
-    public enum WordType
-    {
-        Text,
-        Normalized = Text,
-        Lexical,
-        Pronunciation
-    }
 
 
 }//namespace
